04a99a04f95a174407506d24c0f6cb67db3f493c
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-03-03 20:12:47 +0900

    hotfix: Import 문제 해결#2

--- lib/presentation/provider/sample_provider.dart
+++ lib/presentation/provider/sample_provider.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:yeohaeng_ttukttak_v3/data/dto/place_list_response.dart';
+import 'package:yeohaeng_ttukttak_v3/data/repository/response/place_list_response.dart';
 import 'package:yeohaeng_ttukttak_v3/data/repository/place_repository.dart';
 import 'package:yeohaeng_ttukttak_v3/domain/model/place_model.dart';
 
